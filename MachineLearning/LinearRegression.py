3a9e969971a0d92fde14b5fb3381bcd7b91851f6
RebelReindeer <98460745+RebelReindeer@users.noreply.github.com>
2022-02-14 09:46:36 +0200

    Create LinearRegression.py

---
+++ MachineLearning/LinearRegression.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data = {'House_Size':[100, 200, 220, 300, 700, 800],
+        'price' : [100000, 120000, 130000, 160000, 300000, 400000]}
+
+#M is the size of the dataset
+M = 
+plt.plot(data['House_Size'], data['price'])
+plt.show()
+
+
+
+def hypothesis(theta0, theta1, x):
+    prediction = theta0 + theta1*x
+    return prediction
+
+def cost_function(hypothesis):
+    
+
