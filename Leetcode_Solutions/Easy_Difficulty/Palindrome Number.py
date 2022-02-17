1d855b5c6dd6128c41871d79b6bbc94e00476a03
RebelReindeer <98460745+RebelReindeer@users.noreply.github.com>
2022-02-17 11:06:43 +0200

    Update Palindrome Number.py

--- Leetcode_Solutions/Easy_Difficulty/Palindrome Number.py
+++ Leetcode_Solutions/Easy_Difficulty/Palindrome Number.py
@@ -6,20 +6,32 @@
 #any number that is between 1-9 is always palindrome
 #numbers that end in zero can not be palindrome
 
+def isPalindrome(x):
+         
+    s_num = str(x)
 
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-      
-        if x > 0 or x%10 == 0 or len(str(x)) == 1:
-          return False
-      
-        s_num = str(x)
-        r_num = ''
-        for i in reversed(s_num):
-            r_num += str(i)
-        if r_num == s_num:
-            return True
-        else:
-            return False
-          
-          
+    if x < 0 or x%10 == 0:
+        return False
+    elif s_num == 1:
+        return True
+
+
+
+    r_num = ''
+    for i in reversed(s_num):
+        r_num += str(i)
+    if r_num == s_num:
+        return True
+    else:
+        return False
+
+ 
+isPalindrome(121)
+    #True
+
+ 
+isPalindrome(-34)
+    #False
+
+isPalindrome(11112222333344443333222111)
+    #False
