e995e5c3c0b76ef0fcdd60e16012e0a6d8d7bb87
RebelReindeer <98460745+RebelReindeer@users.noreply.github.com>
2022-02-17 11:01:51 +0200

    Update Palindrome Number.py

--- Leetcode_Solutions/Easy_Difficulty/Palindrome Number.py
+++ Leetcode_Solutions/Easy_Difficulty/Palindrome Number.py
@@ -3,12 +3,14 @@
 #by turning the number into a string you can reverse it
 #then you can check if the reversed number is == original number
 #negative numbers can not be palindrome
+#any number that is between 1-9 is always palindrome
 #numbers that end in zero can not be palindrome
+
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
       
-        if x > 0 or x%10 == 0:
+        if x > 0 or x%10 == 0 or len(str(x)) == 1:
           return False
       
         s_num = str(x)
