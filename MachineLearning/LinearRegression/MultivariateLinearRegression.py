23c116d3105bbc288a1efffe0b0b7e0806d81efc
RebelReindeer <98460745+RebelReindeer@users.noreply.github.com>
2022-02-20 13:20:00 +0200

    Update MultivariateLinearRegression.py

--- MachineLearning/LinearRegression/MultivariateLinearRegression.py
+++ MachineLearning/LinearRegression/MultivariateLinearRegression.py
@@ -5,7 +5,9 @@ class model:
         self.params = np.random.rand(data_shape[0] + 1, 1)
     
     def hypothesis(self, data):
-        
+ 
+        #this hypothesis takes in a single feature vector
+
         #because our hypothesis is hθ(xⁱ) = θ₀ + θ₁x₁ ... θₙxₙ, and we are multiplying them as matrices(θ Xⁱ),  they need to be of shapes nxm, mxn
         #currently, our parameters are a vector (10, 1) and our features are a vector (9, 1).
         #because we are initially adding θ₀ and not multiplying it by anything, we can add a 1 in the zero'th index of our data, so that it becomes θ₀1 + θ₁x₁ ... θₙxₙ
@@ -17,16 +19,35 @@ class model:
         assert data.shape == self.params.shape, f'data shape is {data.shape} and parameters shape is {self.params.shape}'
 
         #multiply the vectors to get our hypothesis
-        return np.dot(self.params.T, data)
+        return np.round(np.dot(self.params.T, data), 2)
+    
+    def table_hypothesis(self, data):
+        
+        #this hypothesis takes in a row of feature vectors
+
+        #first go through every feature vector and add x0 == 1
+        new_data = np.insert(data,0, np.ones(data.shape[0], int), axis = 1)
+
+        
+
+        return np.round(np.dot(new_data, self.params), 2)
+
+
+       
+
+
 
 data = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 data.resize(9, 1)
 
+data1 = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9],
+                  [10, 11, 12, 13, 14, 15, 16, 17, 18],
+                  [19, 20, 21, 22, 23, 24, 25, 27, 28],
+                  [29, 30, 31, 32, 33, 34, 35, 36, 37]])
+
+
+
 m = model(data.shape)
-
-
-
-print(m.hypothesis(data))
-
-
+print(f'model hypothesis with single vector: \n {m.hypothesis(data)}')
+print(f'model hypothesis with vector row: \n {m.table_hypothesis(data1)}')
